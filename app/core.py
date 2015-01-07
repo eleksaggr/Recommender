34ec322a5f79a9c5abbb1d7200f98683ceaf4c11
Alex Egger <alex.egger96@gmail.com>
2015-01-07 23:44:28 +0100

    Added documentation to the core module.

--- app/core.py
+++ app/core.py
@@ -3,8 +3,41 @@ from operator import itemgetter
 import metric
 
 def recommend(population, targetId, similarity=metric.euclidian, k=3):
+	"""
+	Recommends a movie to a user with the id 'targetId'.
+	The movie will not have been rated by the targeted user yet.
+
+	Parameters
+	==========
+	population -> {'userId' : [Rating(...)]}: A population of ratings to compare to.
+
+	targetId -> int: The id of the targeted user.
+
+	similarity -> function: A function that can calculate the similarity between two user
+							by using their ratings.
+	k -> int: The number of movies the system will recommend.
+	==========
+
+	Throws
+	==========
+	KeyError: If the targeted user is not in the population.
+
+	ValueError: If the population is empty.
+	==========
+
+	Returns
+	==========
+	A list of movies the system recommends.
+	The list will be formatted as follows:
+	[(movieId, value)]
+	==========
+	"""
 	totalScore = {}
 	sumSimilarity = {}
+
+
+	if len(population) == 0:
+		raise ValueError('The population may not be empty.')
 
 	# Check if the targeted user exists in the population.
 	if targetId not in population.keys():
@@ -38,3 +71,4 @@ def recommend(population, targetId, similarity=metric.euclidian, k=3):
 		rankings = sorted(rankings, reverse=True, key=itemgetter(1))
 
 		return rankings[0:k]
+		
