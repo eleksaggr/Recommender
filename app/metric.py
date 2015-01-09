a55e4b13db96e360cba7030467d60a3fe0d93e4c
Alex Egger <alex.egger96@gmail.com>
2015-01-09 17:36:54 +0100

    Added function to compute pearson coefficent.

--- app/metric.py
+++ app/metric.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 def euclidian(x, y):
 	"""
 	Calculates the n-dimensional euclidian distance between x and y.
@@ -19,10 +21,52 @@ def euclidian(x, y):
 	if len(x) == 0 or len(y) == 0:
 		raise KeyError('X and/or Y may not be empty.')
 
-	xMovies = [movie.movieId for movie in x]
-	yMovies = [movie.movieId for movie in y]
-
-	mutual = [movie for movie in xMovies if movie in yMovies]
+	mutual = _mutual(x, y)
 
 	s = sum(((x[i].value - y[i].value) ** 2) for i in range(len(mutual)))
 	return 1/(1 + s ** 0.5)
+
+def pearson(x, y):
+	if len(x) == 0 or len(y) == 0:
+		raise KeyError('X and/or Y may not be empty.')
+
+	mutual = _mutual(x, y)
+
+	# Remove all items that are not in both lists.
+	x = [item for item in x if item.movieId in mutual]
+	y = [item for item in y if item.movieId in mutual]
+
+	# Sort so both lists will be in the same order.
+	x = sorted(x, key=itemgetter(0))
+	y = sorted(y, key=itemgetter(0))
+
+	# Calculate average for both lists.
+	avgX = float(sum(rating.value for rating in x)) / float(len(x))
+	avgY = float(sum(rating.value for rating in y)) / float(len(y))
+
+	print(sum(rating.value for rating in y))
+	print(len(y))
+
+	dP = 0
+	dxSquare = 0
+	dySquare = 0
+	for i in range(len(mutual)):
+		dx = x[i].value - avgX
+		dy = y[i].value - avgY
+		dP += dx * dy
+		dxSquare += dx ** 2
+		dySquare += dy ** 2
+
+	if(dxSquare == 0 or dySquare == 0):
+		raise ValueError('Could not compute Pearson coefficent for given input.')
+
+	return dP / (dxSquare * dySquare) ** 0.5
+
+def _mutual(x, y):
+	if len(x) == 0 or len(y) == 0:
+		raise KeyError('X and/or Y may not be empty.')
+
+	xMovies = [movie.movieId for movie in x]
+	yMovies = [movie.movieId for movie in y]
+
+	return [movie for movie in xMovies if movie in yMovies]
