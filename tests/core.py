3782d6cb86dcaae0f30df854a1b8695e1199e9e3
Alex Egger <alex.egger96@gmail.com>
2015-01-11 18:32:40 +0100

    Rewrote tests. Coverage now 100%. Resolves #3."

--- tests/core.py
+++ tests/core.py
@@ -8,14 +8,32 @@ from app import transform
 class CoreTest(unittest.TestCase):
 
 	def test_Recommend(self):
+
+		# Get data to test with.
 		ml = loader.MongoLoader()
-
 		ml.fetchRefs('development', 'user', '_id')
-
-		p = ml.loadSamples('development', 'rating', 100, 'userId', {'_id' : False, 'userId' : False, 'timestamp': False, 'random' : False} )
-		p.update({1 : ml.loadDataset('development', 'rating', 10, 'userId', {'_id' : False, 'userId' : False, 'timestamp': False, 'random' : False})})
-
+		p = ml.loadSamples('development', 'rating', 20, 'userId', {'_id' : False, 'userId' : False, 'timestamp': False, 'random' : False} )
+		p.update({100 : ml.loadDataset('development', 'rating', 100, 'userId', {'_id' : False, 'userId' : False, 'timestamp': False, 'random' : False})})
 		p = transform.toRatings(p)
 
-		r = core.recommend(p, 1, similarity=metric.euclidian)
-		print(r)
+		# Check if correct amount of films is being retruned.
+		r = core.recommend(p, 100, similarity=metric.euclidian)
+		self.assertEqual(3, len(r))
+
+		r = core.recommend(p, 100, similarity=metric.euclidian, k=5)
+		self.assertEqual(5, len(r))
+
+		# Also try pearson metric.
+		r = core.recommend(p, 100, similarity=metric.pearson)
+		self.assertEqual(3, len(r))
+
+		# Empty population.
+		self.assertRaises(ValueError, core.recommend, {}, 100)
+
+		# User not in population.
+		self.assertRaises(KeyError, core.recommend, p, -1)
+
+		# No mutual ratings.
+		p = {1 : [transform.Rating(1, 1)], 2 : [transform.Rating(2, 1)]}
+		r = core.recommend(p, 1)
+		self.assertEqual(0, len(r))
--- tests/metric.py
+++ tests/metric.py
@@ -4,9 +4,32 @@ from app import metric
 
 class MetricTest(unittest.TestCase):
 
+	def test_Euclidian(self):
+		x = [1, 2, 3]
+		y = [1, 2, 3]
+
+		r = metric.euclidian(x, y)
+		self.assertEqual(1, r)
+
+		x = [1, 2, 3]
+		y = [1, 2]
+
+		self.assertRaises(KeyError, metric.euclidian, x, y)
+
 	def test_Pearson(self):
-		x = [{1 : 1}, {2 : 2}, {3 : 3}]
-		x = [{1 : 1}, {2 : 5}, {3 : 7}]
+		x = [1, 2, 3]
+		y = [1, 5, 7]
 
 		r = metric.pearson(x, y)
 		self.assertEqual(round(r, 3), 0.982)
+
+		x = [0, 1, 2]
+		y = [0, 1]
+
+		# Check if list being of different sizes gets caught.
+		self.assertRaises(KeyError, metric.pearson, x, y)
+
+		x = [1, 1, 1]
+		y = [2, 2, 2]
+
+		self.assertRaises(ValueError, metric.pearson, x, y)
